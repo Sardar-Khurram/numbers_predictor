dffa8654c4f2847f3fb7da9d273eb941b4874b4c
Sardar <sardarkhurram121@gmail.com>
2025-03-16 22:25:30 +0500

    comited

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/header";
+import Header from "./components/Header";
 import Home from "./components/Home";
 import Footer from "./components/Footer";
 import Editor from "./components/Editor";
